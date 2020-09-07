3d677dbbc5083e7dec36dd3344ae9c268bacfeb7
NPway <68852260+NPway@users.noreply.github.com>
2020-09-07 17:54:03 +0200

    Created F/C functions

--- src/index.js
+++ src/index.js
@@ -86,10 +86,38 @@ function getCurrentLocation(event) {
 
 }
 
+
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+
+    let temperatureElement = document.querySelector("#current-temperature");
+    let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#current-temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("London");
